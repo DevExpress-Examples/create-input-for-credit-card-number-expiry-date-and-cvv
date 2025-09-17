c6e14e60a9cd39406ec1a50a0367b6de9552165f
Elena Peskova <116714417+elenapeskova@users.noreply.github.com>
2025-09-17 17:28:34 +0400

    Remove DevExpress Bootstrap version configuration
    Removed configuration for DevExpress Bootstrap version.

--- CS/InputCreditCard/Program.cs
+++ CS/InputCreditCard/Program.cs
@@ -8,9 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDevExpressBlazor();
-builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
-    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
-});
 builder.WebHost.UseWebRoot("wwwroot");
 builder.WebHost.UseStaticWebAssets();
 
